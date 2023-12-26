6c5b1b720175aa357e701c647246f54c8ebb0d37
krishna8452 <krishnachaudhary8452@gmail.com>
2023-12-26 21:58:41 +0545

    swagger for api added

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,182 +1,187 @@
-  const express = require("express");
-  const router = express.Router();
-  const{
-    getAllUsers,
-    getUser,
-    addUser,
-    editUser,
-    deleteUser,
-    userLogin,
-    testing
-  } = require("../modules/user/controller/userController");
-  const {uploadImage} = require("../helper/imageUpload")
-  const {jwtAuthentication} = require("../middleware/jwtAuthentication")
-  const yupValidatorMiddleware = require("../middleware/yupValidatorMiddleware")
-  const {createUserSchema, updataUserSchema, isValidIdSchema, getUserSchema} =require("../modules/user/validation/validator")
+const express = require("express");
+const router = express.Router();
+const {
+  getAllUsers,
+  getUser,
+  addUser,
+  editUser,
+  deleteUser,
+  userLogin,
+  testing,
+} = require("../modules/user/controller/userController");
+const { uploadImage } = require("../helper/imageUpload");
+const { jwtAuthentication } = require("../middleware/jwtAuthentication");
+const yupValidatorMiddleware = require("../middleware/yupValidatorMiddleware");
+const {
+  createUserSchema,
+  updataUserSchema,
+  isValidIdSchema,
+  getUserSchema,
+} = require("../modules/user/validation/validator");
 
-  /**
-   * @swagger
-   * components:
-   *   schemas:
-   *     User:
-   *       type: object
-   *       required:
-   *         - name
-   *         - username
-   *         - password
-   *         - email
-   *       properties:
-   *         id:
-   *           type: string
-   *           format: uuid
-   *           description: Auto-generated user ID
-   *         name:
-   *           type: string
-   *           description: User's name
-   *         username:
-   *           type: string
-   *           description: User's username
-   *         password:
-   *           type: string
-   *           description: User's password
-   *         email:
-   *           type: string
-   *           format: email
-   *           description: User's email
-   *         address:
-   *           type: string
-   *           description: User's address
-   *         phone:
-   *           type: string
-   *           description: User's phone number
-   *         image:
-   *           type: string
-   *           format: binary
-   *           description: User's image
-   *       example:
-   *         name: John Doe
-   *         username: johndoe
-   *         password: secret123
-   *         email: john@example.com
-   *         address: 123 Main St, City
-   *         phone: 9800761234
-   */
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     User:
+ *       type: object
+ *       required:
+ *         - name
+ *         - username
+ *         - password
+ *         - email
+ *       properties:
+ *         id:
+ *           type: string
+ *           format: uuid
+ *           description: Auto-generated user ID
+ *         name:
+ *           type: string
+ *           description: User's name
+ *         username:
+ *           type: string
+ *           description: User's username
+ *         password:
+ *           type: string
+ *           description: User's password
+ *         email:
+ *           type: string
+ *           format: email
+ *           description: User's email
+ *         address:
+ *           type: string
+ *           description: User's address
+ *         phone:
+ *           type: string
+ *           description: User's phone number
+ *         image:
+ *           type: string
+ *           format: binary
+ *           description: User's image
+ *       example:
+ *         name: John Doe
+ *         username: johndoe
+ *         password: secret123
+ *         email: john@example.com
+ *         address: 123 Main St, City
+ *         phone: 9800761234
+ */
 
-  /**
-   * @swagger
-   * /api/users:
-   *   get:
-   *     summary: Get all users
-   *     tags:
-   *       - User
-   *     parameters:
-   *       - in: query
-   *         name: page
-   *         schema:
-   *           type: integer
-   *         description: The page number for pagination (default is 1)
-   *       - in: query
-   *         name: perPage
-   *         schema:
-   *           type: integer
-   *         description: The page number for pagination (default is 5)
-   *     responses:
-   *       '200':
-   *         description: User list retrieved successfully
-   *         content:
-   *           application/json:
-   *             schema:
-   *               type: array
-   *               items:
-   *                 $ref: '#/components/schemas/User'
-   *       '500':
-   *         description: Internal server error
-   */
+/**
+ * @swagger
+ * /api/users:
+ *   get:
+ *     summary: Get all users
+ *     tags:
+ *       - User
+ *     parameters:
+ *       - in: query
+ *         name: page
+ *         schema:
+ *           type: integer
+ *         description: The page number for pagination (default is 1)
+ *       - in: query
+ *         name: perPage
+ *         schema:
+ *           type: integer
+ *         description: The page number for pagination (default is 5)
+ *     responses:
+ *       '200':
+ *         description: User list retrieved successfully
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/User'
+ *       '500':
+ *         description: Internal server error
+ */
 
-  /**
-   * @swagger
-   * /api/users/{id}:
-   *   get:
-   *     summary: Get a user by ID
-   *     tags:
-   *       - User
-   *     parameters:
-   *       - in: path
-   *         name: id
-   *         required: true
-   *         schema:
-   *           type: string
-   *           format: uuid
-   *     responses:
-   *       '200':
-   *         description: User retrieved successfully
-   *         content:
-   *           application/json:
-   *             schema:
-   *               $ref: '#/components/schemas/User'
-   *       '500':
-   *         description: Internal server error
-   *   put:
-   *     summary: Update a user by ID
-   *     tags:
-   *       - User
-   *     parameters:
-   *       - in: path
-   *         name: id
-   *         required: true
-   *         schema:
-   *           type: string
-   *           format: uuid
-   *     requestBody:
-   *       required: true
-   *       content:
-   *         application/json:
-   *           schema:
-   *             $ref: '#/components/schemas/User'
-   *     responses:
-   *       '200':
-   *         description: User updated successfully
-   *         content:
-   *           application/json:
-   *             schema:
-   *               $ref: '#/components/schemas/User'
-   *       '500':
-   *         description: Internal server error
-   */
+/**
+ * @swagger
+ * /api/users/{id}:
+ *   get:
+ *     summary: Get a user by ID
+ *     tags:
+ *       - User
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *           format: uuid
+ *     responses:
+ *       '200':
+ *         description: User retrieved successfully
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/User'
+ *       '500':
+ *         description: Internal server error
+ *   put:
+ *     summary: Update a user by ID
+ *     tags:
+ *       - User
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *           format: uuid
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/User'
+ *     responses:
+ *       '200':
+ *         description: User updated successfully
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/User'
+ *       '500':
+ *         description: Internal server error
+ */
 
-  /**
-   * @swagger
-   * /api/users/login:
-   *   post:
-   *     summary: User login
-   *     tags:
-   *       - User
-   *     requestBody:
-   *       required: true
-   *       content:
-   *         application/json:
-   *           schema:
-   *             type: object
-   *             properties:
-   *               username:
-   *                 type: string
-   *                 description: User's username
-   *               password:
-   *                 type: string
-   *                 description: User's password
-   *             required:
-   *               - username
-   *               - password
-   *     responses:
-   *       '200':
-   *         description: User logged in successfully
-   *         content:
-   *           application/json:
-   *             schema:
-   *               $ref: '#/components/schemas/User'
-   *       '500':
-   *         description: Internal server error
-   */
+/**
+ * @swagger
+ * /api/users/login:
+ *   post:
+ *     summary: User login
+ *     tags:
+ *       - User
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               username:
+ *                 type: string
+ *                 description: User's username
+ *               password:
+ *                 type: string
+ *                 description: User's password
+ *             required:
+ *               - username
+ *               - password
+ *     responses:
+ *       '200':
+ *         description: User logged in successfully
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/User'
+ *       '500':
+ *         description: Internal server error
+ */
 
 /**
  * @swagger
@@ -220,39 +225,48 @@
  *     description: Bearer token in the format "Bearer {token}"
  */
 
+/**
+ * @swagger
+ * paths:
+ *   /api/test:
+ *     post:
+ *       summary: Create a new test
+ *       description: Endpoint to create a new test.
+ *       tags:
+ *         - Testing
+ *       consumes:
+ *         - application/json
+ *       produces:
+ *         - application/json
+ *       parameters:
+ *         - in: body
+ *           name: body
+ *           description: Test data to be created (in JSON format)
+ *           required: true
+ *           schema: {}
+ *       responses:
+ *         '201':
+ *           description: Test created successfully
+ *         '400':
+ *           description: Bad request, check your request payload
+ *         '500':
+ *           description: Internal server error
+ */
 
-  /**
-   * @swagger 
-   * /api/users:
-   *   post:
-   *     summary: Add a new user
-   *     tags:
-   *       - User
-   *     requestBody:
-   *       required: true
-   *       content:
-   *         application/json:
-   *           schema:
-   *             $ref: '#/components/schemas/User'
-   *     responses:
-   *       '200':
-   *         description: User added successfully
-   *         content:
-   *           application/json:
-   *             schema:
-   *               $ref: '#/components/schemas/User'
-   *       '500':
-   *         description: Internal server error
-   */
+router.route("/test").post(testing);
+router.route("/users/login").post(userLogin);
+router.route("/users").get(yupValidatorMiddleware(getUserSchema), getAllUsers);
+router.route("/users").post(yupValidatorMiddleware(createUserSchema), addUser);
+router
+  .route("/users/:id")
+  .get(yupValidatorMiddleware(isValidIdSchema), getUser);
+router
+  .route("/users/:id")
+  .put(yupValidatorMiddleware(updataUserSchema), editUser);
 
-  router.route("/test").post(testing);
-  router.route("/users/login").post(userLogin);
-  router.route("/users").get(yupValidatorMiddleware(getUserSchema), getAllUsers);
-  router.route("/users").post(yupValidatorMiddleware(createUserSchema), addUser);
-  router.route("/users/:id").get(yupValidatorMiddleware(isValidIdSchema), getUser);
-  router.route("/users/:id").put(yupValidatorMiddleware(updataUserSchema), editUser);
+router.use(jwtAuthentication);
+router
+  .route("/users/:id")
+  .delete(yupValidatorMiddleware(isValidIdSchema), deleteUser);
 
-  router.use(jwtAuthentication)
-  router.route("/users/:id").delete(yupValidatorMiddleware(isValidIdSchema), deleteUser);
-
-  module.exports = router;
+module.exports = router;
